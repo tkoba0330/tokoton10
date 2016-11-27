320a3008da14c0e54a4ff438f885668a63119d3f
kobayashi tomoaki <t.koba0330@hotmail.co.jp>
2016-11-27 22:34:42 +0900

    とことんC# 10終了

---
+++ tokoton10_07/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tokoton10_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new[] { 1, 2 };
+
+            Console.WriteLine(a.First());
+
+            Console.ReadLine();
+        }
+    }
+}
